597c5a1079ba0ec9eef183636f02e47a0f0bcea1
hari1538radha <hari.r2020ece@sece.ac.in>
2023-01-29 12:46:12 +0530

    check

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -1,6 +1,6 @@
 import pkg from 'express';
 const { express } = pkg;
 import { AddProduct } from "../Controller/Add.js";
-const Routes = pkg.Router;
+const Routes = pkg.Router();
 Routes.post("/add", AddProduct);
 export default Routes
