5d3873e6db64697dd8670d029d8ad644b7bdfc0e
hari1538radha <hari.r2020ece@sece.ac.in>
2023-02-06 12:23:08 +0530

    feat : login Api creted

---
+++ Controller/Login.js
@@ -0,0 +1,32 @@
+import { LoginData } from "../Schema/Schema";
+
+export const AdminLogin = (req, res) => {
+  const body = req.body;
+  if (!body.AdminName) {
+    res.send({
+      status: 204,
+      message: "please enter the Login data completely",
+    });
+  } else {
+    const LoginData = new LoginData({
+      AdminName: body.AdminName,
+      Password: body.Password,
+    });
+    const data =  AdminLogin.findOne({ AdminName: body.AdminName });
+
+    if (data) {
+      const validPassword = compare(
+        body.Password,
+        data.Password
+      );
+      if (validPassword) {
+        res.status(200).send({ message: "Login success", data });
+      } else {
+        res.status(200).send({ error: "Invalid Credentials" });
+      }
+    } else {
+      res.status(401).send({ error: "User does not exist" });
+    }
+  };
+  }
+
--- Routes/Routes.js
+++ Routes/Routes.js
@@ -1,11 +1,13 @@
 import pkg from "express";
 import { AddProduct } from "../Controller/AddProducts.js";
 import { productDetails } from "../Controller/GetProducts.js";
+import { AdminLogin } from "../Controller/Login.js";
 
 const Routes = pkg.Router();
 const { express } = pkg;
 
 Routes.post("/add", AddProduct);
 Routes.get("/products",productDetails)
+Routes.get("/login",AdminLogin)
 
 export default Routes;
