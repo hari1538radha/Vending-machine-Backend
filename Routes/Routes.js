9229b718ad5d28053cbf6a0d70dc9f02542b798a
hari1538radha <hari.r2020ece@sece.ac.in>
2023-02-26 12:18:19 +0530

    fix :check

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -2,7 +2,7 @@ import pkg from "express";
 import { AddProduct } from "../Controller/AddProducts.js";
 import { productDetails } from "../Controller/GetProducts.js";
 import { AdminLogin } from "../Controller/Login.js";
-import { CheckoutProductsInfo } from "../Controller/CheckoutProduct.js";
+import { CheckoutProductsInfo } from "../Controller/CheckOut.js";
 import { Payment } from "../Controller/PaymentControllerr.js";
 
 const Routes = pkg.Router();
